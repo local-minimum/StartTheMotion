ae3fb720657b793cf010b737778989af34978e07
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-04-29 23:13:37 +0200

    Moving on beziers

---
+++ Assets/Scripts/BezierPoint.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class BezierPoint : MonoBehaviour {
+
+    [SerializeField]
+    BezierCurve curve;
+
+    List<BezierZone> inZones = new List<BezierZone>();
+    List<BezierZone> tmpZones = new List<BezierZone>();
+
+    private void OnEnable()
+    {
+        CurveTime = curveTime;
+    }
+
+    private void OnDisable()
+    {
+        inZones.Clear();
+    }
+
+    public bool IsOnCurve(BezierCurve curve)
+    {
+        return curve == this.curve;
+    }
+
+    [SerializeField]
+    float curveTime;
+
+    public float CurveTime
+    {
+        get
+        {
+            return curveTime;
+        }
+
+        set
+        {
+            curveTime = Mathf.Clamp01(value);
+            transform.position = curve.GetGlobalPoint(curveTime);
+            if (curveTime == 0 || curveTime == 1)
+            {
+                BroadcastMessage("OnBezierEnd", this, SendMessageOptions.DontRequireReceiver);
+            }
+
+            BezierZone.PointIsInZones(this, tmpZones);
+            for(int i=0, l=inZones.Count; i<l;i++)
+            {
+                if (!tmpZones.Contains(inZones[i]))
+                {
+                    BroadcastMessage("OnBezierZoneExit", inZones[i], SendMessageOptions.DontRequireReceiver);
+                }
+            }
+
+            for (int i=0, l=tmpZones.Count; i<l; i++)
+            {
+                if (inZones.Contains(tmpZones[i]))
+                {
+                    BroadcastMessage("OnBezierZoneStay", tmpZones[i], SendMessageOptions.DontRequireReceiver);
+                } else
+                {
+                    BroadcastMessage("OnBezierZoneEnter", tmpZones[i], SendMessageOptions.DontRequireReceiver);
+                }
+            }
+            inZones.Clear();
+            inZones.AddRange(tmpZones);
+        }
+    }
+
+    public bool snapping;
+
+    private void Update()
+    {
+        if (snapping && curve != null)
+        {
+            transform.position = curve.GetGlobalPoint(curveTime);
+        }
+    }
+}
--- Assets/Scripts/BezierZone.cs
+++ Assets/Scripts/BezierZone.cs
@@ -4,6 +4,35 @@ using UnityEngine;
 
 public class BezierZone : MonoBehaviour {
 
+    static List<BezierZone> zones = new List<BezierZone>();
+
+    private void OnEnable()
+    {
+        zones.Add(this);
+    }
+
+    private void OnDisable()
+    {
+        zones.Remove(this);
+    }
+
+    private void OnDestroy()
+    {
+        zones.Remove(this);
+    }
+
+    public static void PointIsInZones(BezierPoint pt, List<BezierZone> myZones)
+    {
+        myZones.Clear();   
+        for (int i=0, l=zones.Count; i<l; i++)
+        {
+            if (pt.IsOnCurve(zones[i].curve) && zones[i].IsInside(pt.CurveTime))
+            {
+                myZones.Add(zones[i]);
+            }
+        }
+    }
+
     public float[] times = new float[2] { 0, 1 };
     public float left
     {
@@ -22,6 +51,11 @@ public class BezierZone : MonoBehaviour {
     }
 
     public BezierCurve curve;
+
+    public bool IsInside(float t)
+    {
+        return t >= left && t <= right;
+    }
 
 #if UNITY_EDITOR
 
