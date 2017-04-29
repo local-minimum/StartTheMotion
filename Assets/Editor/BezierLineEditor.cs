08c24d259421ca4e5c0499169a6e2568605f86a4
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-04-29 15:59:45 +0200

    Doesn't work

---
+++ Assets/Editor/BezierLineEditor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(BezierLine))]
+public class BezierLineEditor : Editor {
+
+    Transform handleTransform;
+    Quaternion handleRotation;
+    BezierLine line;
+
+    private void OnSceneGUI()
+    {
+        line = target as BezierLine;
+
+        handleTransform = line.transform;
+        handleRotation = Tools.pivotRotation == PivotRotation.Local ?
+            handleTransform.rotation : Quaternion.identity;
+
+        DrawHandles(line.pt0, "pt2");
+        DrawHandles(line.pt1, "pt1");
+
+        Handles.color = Color.white;
+        Handles.DrawLine(line.pt0, line.pt1);
+    }
+
+    void DrawHandles(Vector3 point, string target)
+    {
+        Vector3 pointGlobal = handleTransform.TransformPoint(point);
+        EditorGUI.BeginChangeCheck();
+        Handles.DoPositionHandle(pointGlobal, handleRotation);
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(line, "Moved Point");
+            EditorUtility.SetDirty(line);
+            line.pt1 = handleTransform.InverseTransformPoint(pointGlobal);
+            /*
+            typeof(BezierLine).GetProperty(target).SetValue(
+                line,
+                handleTransform.InverseTransformPoint(pointGlobal),
+                null);
+            */
+            Debug.Log("Moved Point " + line.pt1);
+            serializedObject.ApplyModifiedProperties();
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/BezierLine.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BezierLine : MonoBehaviour {
+    [SerializeField, HideInInspector]
+    Vector3[] points = new Vector3[3];
+
+    
+    public Vector3 pt0
+    {
+        get
+        {
+            return points[0];
+        }
+
+        set
+        {
+            points[0] = value;
+        }
+    }
+
+    public Vector3 pt1
+    {
+        get
+        {
+            return points[1];
+        }
+
+        set
+        {
+            points[1] = value;
+        }
+    }
+
+}
