81b580691ff94165e24126422df7df6e1380581b
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-04-29 14:05:17 +0200

    testing sequencing and autocleaning

--- Assets/Scripts/ImageCleaner.cs
+++ Assets/Scripts/ImageCleaner.cs
@@ -1,6 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+
+public static class ColorMath
+{
+    public static float Distance(this Color a, Color b)
+    {
+        return Mathf.Abs(a.r - b.r) + Mathf.Abs(a.g - b.g) + Mathf.Abs(a.b - b.b); 
+    }
+}
 
 public struct CoordRect
 {
@@ -50,6 +59,14 @@ public struct CoordRect
         m_h = h;
     }
 
+    public CoordRect(Texture2D tex)
+    {
+        m_x = 0;
+        m_y = 0;
+        m_w = tex.width;
+        m_h = tex.height;
+    }
+
     public bool IsInside(int x, int y)
     {
         return m_x <= x && m_y <= y && m_x + m_w > x && m_y + m_h > y;
@@ -61,7 +78,8 @@ public struct CoordRect
     }
 }
 
-public class ImageCleaner : MonoBehaviour {
+public class ImageCleaner : MonoBehaviour
+{
 
     [SerializeField]
     Texture2D emptyScene;
@@ -70,24 +88,131 @@ public class ImageCleaner : MonoBehaviour {
     Texture2D[] sequence;
 
     [SerializeField]
-    float maxColorDistance = 0.1f;
+    float colorDistanceThreshold = 0.1f;
+
+    [SerializeField]
+    string outputDirectory;
+
+    [SerializeField]
+    bool cropImage;
 
     public void CleanUp()
     {
-        var diff = GetDifferential(0);
-        Debug.Log(BoxTrue(diff));
+        for(int index = 0; index < sequence.Length; index++)
+        {
+            CleanUp(index);
+        }
+
+    }
+
+    void CleanUp(int index)
+    {
+        var diff = GetDifferential(index);
+
+        CoordRect clipBox;
+        if (cropImage)
+        {
+            clipBox = BoxTrue(diff);
+        }
+        else
+        {
+            clipBox = new CoordRect(sequence[index]);
+        }
+
+        var newTex = Clone(index, diff, clipBox);
+        Save(newTex, index, clipBox);
     }
 
     public bool[,] GetDifferential(int index)
     {
         Texture2D tex = sequence[index];
         bool[,] diff = new bool[tex.width, tex.height];
+        for (int x = 0; x < tex.width; x++)
+        {
+            for (int y = 0; y < tex.height; y++)
+            {                
 
+                if (tex.GetPixel(x, y).Distance(emptyScene.GetPixel(x, y)) > colorDistanceThreshold)
+                {
+                    diff[x, y] = true;
+
+                }
+            }
+        }
         return diff;
     }
 
-    static CoordRect BoxTrue(bool[,] data)
+    CoordRect BoxTrue(bool[,] data)
     {
-        return new CoordRect();
+        int w = data.GetLength(0);
+        int h = data.GetLength(1);
+        int minX = -1;
+        int minY = -1;
+        int maxX = 0;
+        int maxY = 0;
+
+        for (int x = 0; x < w; x++)
+        {
+            for (int y = 0; y < h; y++)
+            {
+                if (data[x, y])
+                {
+                    if (minX < 0 || x < minX)
+                    {
+                        minX = x;
+                    }
+                    if (minY < 0 || y < minY)
+                    {
+                        minY = y;
+                    }
+                    if (y > maxY)
+                    {
+                        maxY = y;
+                    }
+                    if (x > maxX)
+                    {
+                        maxX = x;
+                    }
+                }
+            }
+
+        }
+        minX = Mathf.Max(minX, 0);
+        minY = Mathf.Max(minY, 0);
+        maxX = Mathf.Max(maxX, minX);
+        maxY = Mathf.Max(maxY, minY);
+
+        return new CoordRect(minX, minY, maxX - minX, maxY - minY);
+    }
+
+    Texture2D Clone(int sourceIndex, bool[,] filter, CoordRect cropping)
+    {
+        var outTex = new Texture2D(cropping.Width, cropping.Height);
+        var inTex = sequence[sourceIndex];
+
+        for (int inX = cropping.X, outX = 0; inX < outTex.width; inX++, outX++)
+        {
+            for (int inY = cropping.Y, outY = 0; outY < outTex.height; inY++, outY++)
+            {
+                Color c = inTex.GetPixel(inX, inY);
+                if (!filter[inX, inY])
+                {
+                    c.a = 0;
+                }
+                outTex.SetPixel(outX, outY, c);
+            }
+        }
+
+        outTex.Apply();
+        return outTex;
+    }
+
+    void Save(Texture2D tex, int source, CoordRect box)
+    {
+        string targetPath = outputDirectory + sequence[source].name + "-clean.png";
+        Debug.Log(string.Format("{0}: Saving '{1}' using box {2}", source, targetPath, box));
+        byte[] bytes = tex.EncodeToPNG();
+        File.WriteAllBytes(outputDirectory + sequence[source].name + "-clean.png", bytes);
     }
 }
+
---
+++ Assets/Scripts/StopMotionSequencer.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopMotionSequencer : MonoBehaviour {
+
+    SpriteRenderer sRend;
+
+    [SerializeField]
+    Sprite[] sequence;
+
+    [SerializeField]
+    bool[] enabledAnimationStep;
+
+    int showingIndex;
+
+    bool animating;
+
+	public void Step()
+    {
+        animating = false;
+        SetNextFrame();
+        sRend.sprite = sequence[showingIndex];
+    }
+
+    [SerializeField] float m_fps = 15;
+
+    private void Start()
+    {
+        sRend = GetComponent<SpriteRenderer>();
+        if (!sRend)
+        {
+            sRend = gameObject.AddComponent<SpriteRenderer>();
+        }
+
+        if (enabledAnimationStep.Length < sequence.Length)
+        {
+            bool[] tmpArr = new bool[sequence.Length];
+            System.Array.Copy(enabledAnimationStep, tmpArr, enabledAnimationStep.Length);
+            for (int i=enabledAnimationStep.Length; i<tmpArr.Length; i++)
+            {
+                tmpArr[i] = true;
+            }
+            enabledAnimationStep = tmpArr;
+        }
+        Play();
+    }
+
+    public void Play()
+    {
+        if (!animating)
+        {
+            StartCoroutine(Animate(m_fps));
+        }
+    }
+
+    public void Stop()
+    {
+        animating = false;  
+
+    }
+
+    public void SetAnimationStepEnabled(int index)
+    {
+        enabledAnimationStep[index] = true;
+    }
+
+    public void SetAnimationStepDisabled(int index)
+    {
+        enabledAnimationStep[index] = false;
+    }
+
+    IEnumerator<WaitForSeconds> Animate(float fps)
+    {
+        float delta = 1f / fps;
+        animating = true;
+        while (animating)
+        {
+            SetNextFrame();
+            sRend.sprite = sequence[showingIndex];
+            yield return new WaitForSeconds(delta);
+        }
+    }
+
+    void SetNextFrame()
+    {
+        int start = showingIndex;
+
+        do
+        {
+            showingIndex++;
+            showingIndex %= sequence.Length;
+            if (showingIndex == start)
+            {
+                throw new System.ArgumentException("No frames enabled on " + name);
+            }
+        } while (!enabledAnimationStep[showingIndex]);
+    }
+}
