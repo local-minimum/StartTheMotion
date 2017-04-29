e48773a75c32b8627718c53b70bbcaa4849c252d
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-04-29 11:52:55 +0200

    Bones for image cleaner

---
+++ Assets/Editor/ImageCleanerEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ImageCleaner))]
+public class ImageCleanerEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if (GUILayout.Button("Clean Up"))
+        {
+            (target as ImageCleaner).CleanUp();
+        }
+    }
+}
