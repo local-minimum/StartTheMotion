f916ea7ee508d40b7977e0e04c03e24c8ea05f7e
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-05-06 16:01:32 +0200

    working on fish motion

---
+++ Assets/Scripts/ProbabilityAnimTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProbabilityAnimTrigger : MonoBehaviour {
+
+    [SerializeField]
+    string trigger;
+
+    [SerializeField, Range(0, 1)]
+    float probPerSecond;
+
+    [SerializeField]
+    string requiredState;
+
+    StopMotionAnimator smAnim;
+
+    private void Start()
+    {
+        smAnim = GetComponent<StopMotionAnimator>();
+    }
+
+    private void Update()
+    {
+        if (Random.value < probPerSecond * Time.deltaTime)
+        {
+            if (string.IsNullOrEmpty(requiredState) || smAnim.ActiveName == requiredState)
+            {
+                smAnim.Trigger(trigger);
+            }        
+        }
+    }
+}
