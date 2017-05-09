7ea2dd079c656749fb9857be68259732f14bd57b
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-05-09 19:52:06 +0200

    Improving offspring

--- Assets/Scripts/BabyCtrl.cs
+++ Assets/Scripts/BabyCtrl.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BabyCtrl : MonoBehaviour {
 
     [SerializeField]
-    Transform holdingHand;
+    Transform theirHand;
 
     [SerializeField]
     Transform selfRoot;
@@ -16,20 +16,41 @@ public class BabyCtrl : MonoBehaviour {
     [SerializeField, Range(0, 30)]
     float snappiness = 10f;
 
-    [SerializeField, Range(0, 4)]
+    [SerializeField, Range(0, 10)]
     float animationProb = 0.1f;
 
     StopMotionAnimator stAnim;
 
+    StopMotionAnimator theirStAnim;
+
+    Vector3 localScale;
 	// Use this for initialization
 	void Start () {
-        stAnim = GetComponent<StopMotionAnimator>();	
+        localScale = transform.localScale;
+        stAnim = GetComponent<StopMotionAnimator>();
+        theirStAnim = theirHand.GetComponentInParent<StopMotionAnimator>();
 	}
 	
 	// Update is called once per frame
+    bool TheyAllowBounce
+    {
+        get
+        {
+            return !(string.IsNullOrEmpty(theirStAnim.ActiveName) || theirStAnim.ActiveName.StartsWith("Idle"));
+        }
+    }
+
+    float transformXDirection
+    {
+        get
+        {
+            return !TheyAllowBounce ? Mathf.Sign(theirStAnim.transform.localScale.x) : -1f;
+        }
+    }
+
 	void Update () {
 
-	    if (Random.value < animationProb * Time.deltaTime)
+	    if (TheyAllowBounce && Random.value < animationProb * Time.deltaTime)
         {
             if (stAnim.ActiveName != "Bounce")
             {
@@ -38,7 +59,9 @@ public class BabyCtrl : MonoBehaviour {
             stAnim.StepToRandomInActive();
         }
 
-        Vector3 offset = holdingHand.position - selfHand.position;
+        Vector3 offset = theirHand.position - selfHand.position;
         selfRoot.position += offset.normalized * Mathf.Lerp(0, offset.magnitude, snappiness * Time.deltaTime);
+        localScale.x = Mathf.Abs(localScale.x) * transformXDirection;
+        transform.localScale = localScale;
 	}
 }
