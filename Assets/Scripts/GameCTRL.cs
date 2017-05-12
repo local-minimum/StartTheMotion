58f0aef03895db112f3371c82c5f1c4c4f881877
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-05-13 00:57:03 +0200

    background and more

---
+++ Assets/Scripts/GameCTRL.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameCTRL : MonoBehaviour {
+
+   
+	// Update is called once per frame
+	void Update () {
+
+	    if(Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("waterWorld");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+	}
+}
